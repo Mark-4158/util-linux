0989f4c8e14c5b3291c3b4cfab452845b1849efc
Mark A. Williams, Jr. <rpwk5lk0@duck.com>
2024-10-06 13:36:42 -0500

    flock: add support for POSIX locks and file leases
    Directly expose the F_SETLK/F_SETLKW and F_SETLEASE after F_SETSIG commands via
    a --posix and a --lease[=<signal>] option respectively. These new options, which
    imply the --no-fork and --nonblock options respectively, cause flock(1) to pass
    a POSIX lock (in the case of --posix) or file lease (in the case of
    --lease[=<signal>]) to the command to run.
    
    Signed-off-by: Mark A. Williams, Jr. <rpwk5lk0@duck.com>

--- sys-utils/flock.c
+++ sys-utils/flock.c
@@ -47,6 +47,7 @@
 #include "closestream.h"
 #include "monotonic.h"
 #include "timer.h"
+#include "signames.h"
 
 #ifndef F_OFD_GETLK
 #define F_OFD_GETLK	36
@@ -54,9 +55,16 @@
 #define F_OFD_SETLKW	38
 #endif
 
+#ifndef SIGRTMID
+#define SIGRTMID    	((SIGRTMAX - SIGRTMIN + 1) >> 1)
+#endif
+
 enum {
-	API_FLOCK,
-	API_FCNTL_OFD,
+	API_NONE = 0,
+	API_FLOCK = ~API_NONE,
+	API_FCNTL_OFD = LOCK_SH,
+	API_FCNTL_POSIX = LOCK_EX,
+	API_FCNTL_LEASE = LOCK_NB,
 };
 
 static void __attribute__((__noreturn__)) usage(void)
@@ -82,6 +90,8 @@ static void __attribute__((__noreturn__)) usage(void)
 	fputs(_(  " -c, --command <command>  run a single command string through the shell\n"), stdout);
 	fputs(_(  " -F, --no-fork            execute command without forking\n"), stdout);
 	fputs(_(  "     --fcntl              use fcntl(F_OFD_SETLK) rather than flock()\n"), stdout);
+	fputs(_(  "     --posix              use fcntl(F_SETLK) rather than flock() (implies --no-fork)\n"), stdout);
+	fputs(_(  "     --lease[=<signal>]   use fcntl(F_SETLEASE) with fcntl(F_SETSIG) rather than flock() (implies --nonblock)\n"), stdout);
 	fputs(_(  "     --verbose            increase verbosity\n"), stdout);
 	fputs(USAGE_SEPARATOR, stdout);
 	fprintf(stdout, USAGE_HELP_OPTIONS(26));
@@ -152,7 +162,7 @@ static int flock_to_fcntl_type(int op)
 	}
 }
 
-static int fcntl_lock(int fd, int op, int block)
+static int do_lock(int api, int fd, int op, int block)
 {
 	struct flock arg = {
 		.l_type = flock_to_fcntl_type(op),
@@ -160,17 +170,24 @@ static int fcntl_lock(int fd, int op, int block)
 		.l_start = 0,
 		.l_len = 0,
 	};
-	int cmd = (block & LOCK_NB) ? F_OFD_SETLK : F_OFD_SETLKW;
-	return fcntl(fd, cmd, &arg);
-}
 
-static int do_lock(int api, int fd, int op, int block)
-{
-	switch (api) {
+	switch (api | block) {
 	case API_FLOCK:
 		return flock(fd, op | block);
+	case API_FCNTL_LEASE | LOCK_NB:
+		return fcntl(fd, F_SETLEASE, arg.l_type);
 	case API_FCNTL_OFD:
-		return fcntl_lock(fd, op, block);
+		op = F_OFD_SETLKW;
+		break;
+	case API_FCNTL_OFD | LOCK_NB:
+		op = F_OFD_SETLK;
+		break;
+	case API_FCNTL_POSIX:
+		op = F_SETLKW;
+		break;
+	case API_FCNTL_POSIX | LOCK_NB:
+		op = F_SETLK;
+		break;
 	/*
 	 * Should never happen, api can never have values other than
 	 * API_*.
@@ -178,6 +195,8 @@ static int do_lock(int api, int fd, int op, int block)
 	default:
 		errx(EX_SOFTWARE, _("internal error, unknown api %d"), api);
 	}
+
+	return fcntl(fd, op, &arg);
 }
 
 
@@ -195,6 +214,7 @@ int main(int argc, char *argv[])
 	int no_fork = 0;
 	int status;
 	int verbose = 0;
+	int signum = 0;
 	int api = API_FLOCK;
 	struct timeval time_start = { 0 }, time_done = { 0 };
 	/*
@@ -207,6 +227,8 @@ int main(int argc, char *argv[])
 	enum {
 		OPT_VERBOSE = CHAR_MAX + 1,
 		OPT_FCNTL,
+		OPT_POSIX,
+		OPT_LEASE,
 	};
 	static const struct option long_options[] = {
 		{"shared", no_argument, NULL, 's'},
@@ -221,6 +243,8 @@ int main(int argc, char *argv[])
 		{"no-fork", no_argument, NULL, 'F'},
 		{"verbose", no_argument, NULL, OPT_VERBOSE},
 		{"fcntl", no_argument, NULL, OPT_FCNTL},
+		{"posix", no_argument, NULL, OPT_POSIX},
+		{"lease", optional_argument, NULL, OPT_LEASE},
 		{"help", no_argument, NULL, 'h'},
 		{"version", no_argument, NULL, 'V'},
 		{NULL, 0, NULL, 0}
@@ -258,9 +282,46 @@ int main(int argc, char *argv[])
 		case 'o':
 			do_close = 1;
 			break;
+		case OPT_POSIX:
+			api &= API_FCNTL_POSIX;
+			[[fallthrough]];
 		case 'F':
 			no_fork = 1;
 			break;
+		case OPT_LEASE:
+			if (optarg) {
+				ix = 0;
+				sscanf(optarg, "%*[^0-9]%n", &ix);
+				if ((opt = sscanf(optarg + ix, "%d%c", &signum, (char *)&ix)) != 1 || ix) {
+					ix = 0;
+					sscanf(optarg, "SIG%n", &ix);
+					optarg += ix;
+					ix = 0;
+
+					if (opt != 1) {
+						signum = 31;
+						while (strcoll(sigabbrev_np(signum), optarg)) {
+							if (!--signum)
+								goto signame_err;
+						}
+					} else if (signum > SIGRTMID) {
+						errx(EX_USAGE,
+							_("invalid signal specification: %s"),
+							optarg);
+					} else if (sscanf(optarg, "RTMIN+%*[0-9]%n", &ix), ix) {
+						signum += SIGRTMIN;
+					} else if (sscanf(optarg, "RTMAX-%*[0-9]%n", &ix), ix) {
+						signum = SIGRTMAX - signum;
+					} else {
+signame_err:
+						errx(EX_USAGE,
+							_("invalid signal name: %s"),
+							optarg);
+					}
+				}
+			}
+			api &= API_FCNTL_LEASE;
+			[[fallthrough]];
 		case 'n':
 			block = LOCK_NB;
 			break;
@@ -276,7 +337,7 @@ int main(int argc, char *argv[])
 				errx(EX_USAGE, _("exit code out of range (expected 0 to 255)"));
 			break;
 		case OPT_FCNTL:
-			api = API_FCNTL_OFD;
+			api &= API_FCNTL_OFD;
 			break;
 		case OPT_VERBOSE:
 			verbose = 1;
@@ -293,7 +354,10 @@ int main(int argc, char *argv[])
 
 	if (no_fork && do_close)
 		errx(EX_USAGE,
-			_("the --no-fork and --close options are incompatible"));
+			_("--no-fork and --posix are incompatible with --close"));
+	if (!api)
+		errx(EX_USAGE,
+			_("--fcntl, --posix, and --lease are incompatible with each other"));
 
 	/*
 	 * For fcntl(F_OFD_SETLK), an exclusive lock requires that the
@@ -331,6 +395,9 @@ int main(int argc, char *argv[])
 		/* Bad options */
 		errx(EX_USAGE, _("requires file descriptor, file or directory"));
 	}
+
+	if (signum && fcntl(fd, F_SETSIG, signum))
+		errx(EX_USAGE, _("invalid signal number: %d"), signum);
 
 	if (have_timeout) {
 		if (timeout.it_value.tv_sec == 0 &&
